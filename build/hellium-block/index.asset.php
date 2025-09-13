31fb0e942e6e672eec93bb16ef542c73c2a7024b
hazrath <hazrath.bplugins@gmail.com>
2025-09-13 17:37:52 +0600

    add the block in remote repository

---
+++ build/hellium-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eef7d65f12a1042abad4');
---
+++ build/hellium-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b84ee99792bd5d3e3c51');
